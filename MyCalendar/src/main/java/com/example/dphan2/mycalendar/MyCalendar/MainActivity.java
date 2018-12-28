cd1667e39e751ca8eadf8b542e4b6f7a0d857bdb
ptmdmusique <37349324+ptmdmusique@users.noreply.github.com>
2018-12-28 13:55:41 -0800

    28/12/2018 1:55PM
    Initial Commit. Setting up Google Calendar, not finished.

---
+++ MyCalendar/src/main/java/com/example/dphan2/mycalendar/MyCalendar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dphan2.mycalendar.MyCalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
